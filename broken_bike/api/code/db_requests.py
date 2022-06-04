adb240c934b72421cd1d44151e3cd604c1328b3b
Juan <alexandre.juan@epitech.eu>
2022-06-04 23:52:07 +0200

    fix(api): update SQL request to avoid innjection

--- broken_bike/api/code/db_requests.py
+++ broken_bike/api/code/db_requests.py
@@ -15,7 +15,7 @@ class Bikes:
     def add_broken_bike(self, bike_id: int, priority: str, reason: str, description: str) -> None:
         date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         try:
-            self.db.execute(f"INSERT INTO bikes (id_bike, priority, reason, description, created_at) VALUES ({bike_id}, '{priority}', '{reason}', '{description}', '{date}')")
+            self.db.execute("""INSERT INTO bikes (id_bike, priority, reason, description, created_at) VALUES (%s, %s, %s, %s, %s)""", (bike_id, priority, reason, description, date))
             return True
         except Exception as e:
             print(str(e))
@@ -23,7 +23,7 @@ class Bikes:
 
     def remove_bike(self, bike_id: int):
         try:
-            self.db.execute(f"DELETE FROM bikes WHERE id = '{bike_id}'")
+            self.db.execute("""DELETE FROM bikes WHERE id = %s""", (bike_id,))
             return True
         except Exception as e:
             print(str(e))
@@ -36,7 +36,7 @@ class User:
 
     def connect(self, username, password):
         try:
-            result = self.db.execute(f"SELECT * FROM admin WHERE email = '{username}' AND password = '{password}'")
+            result = self.db.execute("""SELECT * FROM admin WHERE email = %s AND password = %s""", (username, password))
             if len(result) > 0:
                 return True
             else:
